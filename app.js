22e42e08f3002da89911bb162c0eef5db4ea964a
Petr Zajkas <petrzajkas@gmail.com>
2023-04-09 23:32:53 +0100

    more JQuery code created and improved on-scroll animation

--- app.js
+++ app.js
@@ -12,3 +12,47 @@ const observer = new IntersectionObserver ((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* Jquery on scroll animations*/ 
+$(document).ready(function () {
+    let offset = {offset: "80%"}
+
+            $(".faded-right").waypoint(
+                    function() {
+                        $(".faded-right").addClass("animate__animated animate__fadeInRight");
+                }, 
+                offset
+            );
+    
+            $("#zoom-in").waypoint(
+                    function() {
+                    $("#zoom-in").addClass("animate__animated animate__zoomIn");
+                }, 
+                offset
+            );
+    
+            $(".animation-pulse").waypoint(
+                    function() {
+                    $(".animation-pulse").addClass("animate__animated animate__pulse");
+                }, 
+                offset
+            );
+
+            $(".faded-left").waypoint(
+                    function() {
+                    $(".faded-left").addClass("animate__animated animate__fadeInLeft");
+                }, 
+                offset
+            );
+
+            $("#heart-beat").waypoint(
+                function() {
+                $("#heart-beat").addClass("animate__animated animate__heartBeat");
+            }, 
+            offset
+            );
+});
+
+
+
+
